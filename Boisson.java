2197e1dc43e1ae3f6c4abd870d6d0443ddab0c17
PaulinDeuxTrois <paulin.rebours@gmail.com>
2021-12-11 14:33:54 +0100

    Boisson

---
+++ Boisson.java
@@ -0,0 +1,16 @@
+public class Boisson implements Frais {
+    public double litre;
+    public boolean gazeux;
+    public String nom;
+    public double poids;
+    public double calories;
+    public Boisson(String nom, boolean gazeux, double litre,double calories){
+        this.litre=litre;
+        this.poids=litre;
+        this.nom=nom;
+        this.calories=calories;
+    }
+    public void unVerre(double litre){
+        this.litre=-litre;
+    }
+}
